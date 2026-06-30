d2257958f44776b46fdac628fb1ab2cdb9104acb
Mohammad Mahdi <MohammadMahdiAkhdar@gmail.com>
2026-06-30 17:30:59 +0300

    prisma.ts file error detection

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,10 +1,15 @@
-// import { PrismaClient } from "@prisma/client";
 import { PrismaClient } from "../generated/prisma";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma =globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
+  globalForPrisma.prisma = prisma;
 }
